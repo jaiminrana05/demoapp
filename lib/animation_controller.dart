0c5f9e59d1809f91e2e3ed3540a2026dd0fcf3e2
jaimin <jaimin.r@simformsolutions.com>
2022-03-16 14:36:55 +0530

    using setState to re-render the state

---
+++ lib/animation_controller.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class AnimationControllerExample extends StatefulWidget {
+  const AnimationControllerExample({Key? key}) : super(key: key);
+
+  @override
+  _AnimationControllerExampleState createState() =>
+      _AnimationControllerExampleState();
+}
+
+class _AnimationControllerExampleState extends State<AnimationControllerExample>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation color;
+  late Animation size;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    color = ColorTween(begin: Colors.blue, end: Colors.red).animate(
+      CurvedAnimation(parent: controller, curve: Curves.fastLinearToSlowEaseIn),
+    );
+    size = Tween<double>(begin: 100.0, end: 200.0).animate(controller);
+
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animations Mystery!'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                width: size.value,
+                height: size.value,
+                color: color.value,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
